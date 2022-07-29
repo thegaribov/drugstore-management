2732b3ab7eaeae4c158c461b5d63e95253d52b70
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-07-29 19:29:16 +0400

    Initial structure completed

---
+++ drugstore-management/drugstore-management/UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace drugstore_management.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
